2041f27f438ccfbed8c413171f7295b06b72313a
hpaasch <hepaasch@gmail.com>
2016-06-20 17:11:20 -0400

    login, logout, register and accountview exist.

--- bank_app/views.py
+++ bank_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView, ListView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class CreateUserView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = "/login/"
+
+
+class AccountView(ListView):
+    model = User
+    template_name = 'account.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AccountView, self).get_context_data(**kwargs)
+        context['user_pk']= self.request.user.pk
+        return context
