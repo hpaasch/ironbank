8963f7e52663c1c9e02afdf56584a8e4e3b9ca73
hpaasch <hepaasch@gmail.com>
2016-06-21 15:37:32 -0400

    balance function pulled out and reused.

--- bank_app/views.py
+++ bank_app/views.py
@@ -7,6 +7,16 @@ import datetime
 
 from bank_app.models import AccountTransaction
 
+def balance(self):
+    trans_list = AccountTransaction.objects.filter(customer__username=self.request.user)
+    self.balance = 0
+    for trans in trans_list:
+        if trans.trans_type == 'Credit':
+            self.balance += trans.trans_amount
+        elif trans.trans_type == 'Debit':
+            self.balance -= trans.trans_amount
+    return self.balance
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -28,15 +38,20 @@ class AccountView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        balance = 0
-        trans_list = AccountTransaction.objects.filter(customer__username=self.request.user)
-        for trans in trans_list:
-            if trans.trans_type == 'Credit':
-                balance += trans.trans_amount
-            elif trans.trans_type == 'Debit':
-                balance -= trans.trans_amount
-        context['balance']= balance
+        context['balance'] = balance(self)
         return context
+
+
+        # context = super().get_context_data(**kwargs)
+        # balance = 0
+        # trans_list = AccountTransaction.objects.filter(customer__username=self.request.user)
+        # for trans in trans_list:
+        #     if trans.trans_type == 'Credit':
+        #         balance += trans.trans_amount
+        #     elif trans.trans_type == 'Debit':
+        #         balance -= trans.trans_amount
+        # context['balance']= balance
+        # return context
 
 
 class TransDetailView(DetailView):
@@ -49,11 +64,10 @@ class TransDetailView(DetailView):
 class CreateTransView(CreateView):
     model = AccountTransaction
     fields = ['trans_amount', 'trans_type', 'trans_note']
-    # success_url = '/accounts/profile'
-    success_url = reverse_lazy('account_view')
-
+    success_url = reverse_lazy('account_view')  # better than /accounts/profile
 
     def form_valid(self, form):
         trans = form.save(commit=False)  # this half saves it
         trans.customer = self.request.user  # this attaches the user in the DB
+        # if trans.trans_type == 'Debit' and 'balance' <= 0:
         return super().form_valid(form)  # this fully creates the transaction
